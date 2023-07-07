9651fd61abb3bbead3f527a1cab219c4e3225453
Renato <renato.gfteixeira@outlook.com>
2023-07-06 23:39:44 -0300

    script.js included

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function switchTheme() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+    const theme = document.body.classList[0];
+    const music = theme === 'light-theme' ? 'normal-world.mpeg' : 'inverted-world.mpeg'
+
+    const audio = document.getElementById('music');
+    audio.src = `assets/musics/${music}`;
+    audio.play();
+    audio.volume = 0.2;
+}
+
+window.addEventListener('click', () => {
+    const audio = document.getElementById('music');
+    audio.play();
+    audio.volume = 0.2;
+})
